06adbb26af9b14cbf025ca5dc8623b80a857150a
letterso <lettersony00@gmail.com>
2025-10-20 10:47:23 +0800

    feat: 优化LRU实现，增加UpdateLRU

--- sparse_voxel_grid/sparse_voxel_grid.cpp
+++ sparse_voxel_grid/sparse_voxel_grid.cpp
@@ -1,6 +1,7 @@
 #include "sparse_voxel_grid.hpp"
 
 #include <Eigen/Core>
+#include <queue>
 #include <array>
 #include <cstdint>
 
@@ -8,6 +9,68 @@
 
 namespace {
 using Bonxai::CoordT;
+
+constexpr std::array<CoordT, 125> shifts{
+    // x = -2 plane
+    // y = -2
+    CoordT{.x = -2, .y = -2, .z = -2}, CoordT{.x = -2, .y = -2, .z = -1}, CoordT{.x = -2, .y = -2, .z = 0}, CoordT{.x = -2, .y = -2, .z = 1}, CoordT{.x = -2, .y = -2, .z = 2},
+    // y = -1
+    CoordT{.x = -2, .y = -1, .z = -2}, CoordT{.x = -2, .y = -1, .z = -1}, CoordT{.x = -2, .y = -1, .z = 0}, CoordT{.x = -2, .y = -1, .z = 1}, CoordT{.x = -2, .y = -1, .z = 2},
+    // y = 0
+    CoordT{.x = -2, .y = 0, .z = -2}, CoordT{.x = -2, .y = 0, .z = -1}, CoordT{.x = -2, .y = 0, .z = 0}, CoordT{.x = -2, .y = 0, .z = 1}, CoordT{.x = -2, .y = 0, .z = 2},
+    // y = 1
+    CoordT{.x = -2, .y = 1, .z = -2}, CoordT{.x = -2, .y = 1, .z = -1}, CoordT{.x = -2, .y = 1, .z = 0}, CoordT{.x = -2, .y = 1, .z = 1}, CoordT{.x = -2, .y = 1, .z = 2},
+    // y = 2
+    CoordT{.x = -2, .y = 2, .z = -2}, CoordT{.x = -2, .y = 2, .z = -1}, CoordT{.x = -2, .y = 2, .z = 0}, CoordT{.x = -2, .y = 2, .z = 1}, CoordT{.x = -2, .y = 2, .z = 2},
+
+    // x = -1 plane
+    // y = -2
+    CoordT{.x = -1, .y = -2, .z = -2}, CoordT{.x = -1, .y = -2, .z = -1}, CoordT{.x = -1, .y = -2, .z = 0}, CoordT{.x = -1, .y = -2, .z = 1}, CoordT{.x = -1, .y = -2, .z = 2},
+    // y = -1
+    CoordT{.x = -1, .y = -1, .z = -2}, CoordT{.x = -1, .y = -1, .z = -1}, CoordT{.x = -1, .y = -1, .z = 0}, CoordT{.x = -1, .y = -1, .z = 1}, CoordT{.x = -1, .y = -1, .z = 2},
+    // y = 0
+    CoordT{.x = -1, .y = 0, .z = -2}, CoordT{.x = -1, .y = 0, .z = -1}, CoordT{.x = -1, .y = 0, .z = 0}, CoordT{.x = -1, .y = 0, .z = 1}, CoordT{.x = -1, .y = 0, .z = 2},
+    // y = 1
+    CoordT{.x = -1, .y = 1, .z = -2}, CoordT{.x = -1, .y = 1, .z = -1}, CoordT{.x = -1, .y = 1, .z = 0}, CoordT{.x = -1, .y = 1, .z = 1}, CoordT{.x = -1, .y = 1, .z = 2},
+    // y = 2
+    CoordT{.x = -1, .y = 2, .z = -2}, CoordT{.x = -1, .y = 2, .z = -1}, CoordT{.x = -1, .y = 2, .z = 0}, CoordT{.x = -1, .y = 2, .z = 1}, CoordT{.x = -1, .y = 2, .z = 2},
+
+    // x = 0 plane (containing the center point (0, 0, 0))
+    // y = -2
+    CoordT{.x = 0, .y = -2, .z = -2}, CoordT{.x = 0, .y = -2, .z = -1}, CoordT{.x = 0, .y = -2, .z = 0}, CoordT{.x = 0, .y = -2, .z = 1}, CoordT{.x = 0, .y = -2, .z = 2},
+    // y = -1
+    CoordT{.x = 0, .y = -1, .z = -2}, CoordT{.x = 0, .y = -1, .z = -1}, CoordT{.x = 0, .y = -1, .z = 0}, CoordT{.x = 0, .y = -1, .z = 1}, CoordT{.x = 0, .y = -1, .z = 2},
+    // y = 0
+    CoordT{.x = 0, .y = 0, .z = -2}, CoordT{.x = 0, .y = 0, .z = -1}, CoordT{.x = 0, .y = 0, .z = 0}, CoordT{.x = 0, .y = 0, .z = 1}, CoordT{.x = 0, .y = 0, .z = 2},
+    // y = 1
+    CoordT{.x = 0, .y = 1, .z = -2}, CoordT{.x = 0, .y = 1, .z = -1}, CoordT{.x = 0, .y = 1, .z = 0}, CoordT{.x = 0, .y = 1, .z = 1}, CoordT{.x = 0, .y = 1, .z = 2},
+    // y = 2
+    CoordT{.x = 0, .y = 2, .z = -2}, CoordT{.x = 0, .y = 2, .z = -1}, CoordT{.x = 0, .y = 2, .z = 0}, CoordT{.x = 0, .y = 2, .z = 1}, CoordT{.x = 0, .y = 2, .z = 2},
+
+    // x = 1 plane
+    // y = -2
+    CoordT{.x = 1, .y = -2, .z = -2}, CoordT{.x = 1, .y = -2, .z = -1}, CoordT{.x = 1, .y = -2, .z = 0}, CoordT{.x = 1, .y = -2, .z = 1}, CoordT{.x = 1, .y = -2, .z = 2},
+    // y = -1
+    CoordT{.x = 1, .y = -1, .z = -2}, CoordT{.x = 1, .y = -1, .z = -1}, CoordT{.x = 1, .y = -1, .z = 0}, CoordT{.x = 1, .y = -1, .z = 1}, CoordT{.x = 1, .y = -1, .z = 2},
+    // y = 0
+    CoordT{.x = 1, .y = 0, .z = -2}, CoordT{.x = 1, .y = 0, .z = -1}, CoordT{.x = 1, .y = 0, .z = 0}, CoordT{.x = 1, .y = 0, .z = 1}, CoordT{.x = 1, .y = 0, .z = 2},
+    // y = 1
+    CoordT{.x = 1, .y = 1, .z = -2}, CoordT{.x = 1, .y = 1, .z = -1}, CoordT{.x = 1, .y = 1, .z = 0}, CoordT{.x = 1, .y = 1, .z = 1}, CoordT{.x = 1, .y = 1, .z = 2},
+    // y = 2
+    CoordT{.x = 1, .y = 2, .z = -2}, CoordT{.x = 1, .y = 2, .z = -1}, CoordT{.x = 1, .y = 2, .z = 0}, CoordT{.x = 1, .y = 2, .z = 1}, CoordT{.x = 1, .y = 2, .z = 2},
+
+    // x = 2 plane
+    // y = -2
+    CoordT{.x = 2, .y = -2, .z = -2}, CoordT{.x = 2, .y = -2, .z = -1}, CoordT{.x = 2, .y = -2, .z = 0}, CoordT{.x = 2, .y = -2, .z = 1}, CoordT{.x = 2, .y = -2, .z = 2},
+    // y = -1
+    CoordT{.x = 2, .y = -1, .z = -2}, CoordT{.x = 2, .y = -1, .z = -1}, CoordT{.x = 2, .y = -1, .z = 0}, CoordT{.x = 2, .y = -1, .z = 1}, CoordT{.x = 2, .y = -1, .z = 2},
+    // y = 0
+    CoordT{.x = 2, .y = 0, .z = -2}, CoordT{.x = 2, .y = 0, .z = -1}, CoordT{.x = 2, .y = 0, .z = 0}, CoordT{.x = 2, .y = 0, .z = 1}, CoordT{.x = 2, .y = 0, .z = 2},
+    // y = 1
+    CoordT{.x = 2, .y = 1, .z = -2}, CoordT{.x = 2, .y = 1, .z = -1}, CoordT{.x = 2, .y = 1, .z = 0}, CoordT{.x = 2, .y = 1, .z = 1}, CoordT{.x = 2, .y = 1, .z = 2},
+    // y = 2
+    CoordT{.x = 2, .y = 2, .z = -2}, CoordT{.x = 2, .y = 2, .z = -1}, CoordT{.x = 2, .y = 2, .z = 0}, CoordT{.x = 2, .y = 2, .z = 1}, CoordT{.x = 2, .y = 2, .z = 2}
+};
 
 // constexpr std::array<Bonxai::CoordT, 27> shifts{
 //     CoordT{.x = -1, .y = -1, .z = -1}, CoordT{.x = -1, .y = -1, .z = 0}, CoordT{.x = -1, .y = -1, .z = 1},
@@ -22,30 +85,26 @@ using Bonxai::CoordT;
 //     CoordT{.x = 1, .y = 0, .z = -1},   CoordT{.x = 1, .y = 0, .z = 0},   CoordT{.x = 1, .y = 0, .z = 1},
 //     CoordT{.x = 1, .y = 1, .z = -1},   CoordT{.x = 1, .y = 1, .z = 0},   CoordT{.x = 1, .y = 1, .z = 1}};
 
-constexpr std::array<Bonxai::CoordT, 19> shifts{
-    CoordT{.x = 0, .y = 0, .z = 0}, CoordT{.x = -1, .y = 0, .z = 0}, CoordT{.x = 1, .y = 0, .z = 0}, CoordT{.x = 0, .y = 1, .z = 0},  
-    CoordT{.x = 0, .y = -1, .z = 0},  CoordT{.x = 0, .y = 0, .z = -1}, CoordT{.x = 0, .y = 0, .z = 1},  CoordT{.x = 1, .y = 1, .z = 0},  
-    CoordT{.x = -1, .y = 1, .z = 0},  CoordT{.x = 1, .y = -1, .z = 0}, CoordT{.x = -1, .y = -1, .z = 0},  CoordT{.x = 1, .y = 0, .z = 1}, 
-    CoordT{.x = -1, .y = 0, .z = 1},  CoordT{.x = 1, .y = 0, .z = -1}, CoordT{.x = -1, .y = 0, .z = -1},  CoordT{.x = 0, .y = 1, .z = 1},
-    CoordT{.x = 0, .y = -1, .z = 1},  CoordT{.x = 0, .y = 1, .z = -1}, CoordT{.x = 0, .y = -1, .z = -1} };
+// constexpr std::array<Bonxai::CoordT, 19> shifts{
+//     CoordT{.x = 0, .y = 0, .z = 0}, CoordT{.x = -1, .y = 0, .z = 0}, CoordT{.x = 1, .y = 0, .z = 0}, CoordT{.x = 0, .y = 1, .z = 0},  
+//     CoordT{.x = 0, .y = -1, .z = 0},  CoordT{.x = 0, .y = 0, .z = -1}, CoordT{.x = 0, .y = 0, .z = 1},  CoordT{.x = 1, .y = 1, .z = 0},  
+//     CoordT{.x = -1, .y = 1, .z = 0},  CoordT{.x = 1, .y = -1, .z = 0}, CoordT{.x = -1, .y = -1, .z = 0},  CoordT{.x = 1, .y = 0, .z = 1}, 
+//     CoordT{.x = -1, .y = 0, .z = 1},  CoordT{.x = 1, .y = 0, .z = -1}, CoordT{.x = -1, .y = 0, .z = -1},  CoordT{.x = 0, .y = 1, .z = 1},
+//     CoordT{.x = 0, .y = -1, .z = 1},  CoordT{.x = 0, .y = 1, .z = -1}, CoordT{.x = 0, .y = -1, .z = -1} };
 
 constexpr uint8_t inner_grid_log2_dim = 2;
 constexpr uint8_t leaf_grid_log2_dim = 3;
 } // namespace
 
-
-namespace rko_lio::core {
-
-SparseVoxelGrid::SparseVoxelGrid(const double voxel_size,
+IVdb::IVdb(const double voxel_size,
                                  const unsigned int max_points_per_voxel,
                                  const size_t capacity)
     : voxel_size_(voxel_size),
       max_points_per_voxel_(max_points_per_voxel),
       capacity_(capacity),
-      update_count(0),
       map_(voxel_size, inner_grid_log2_dim, leaf_grid_log2_dim) {}
 
-bool SparseVoxelGrid::GetClosestPoint(const PointType& point,
+bool IVdb::GetClosestPoint(const PointType& point,
                                       PointVector& closest_point, int max_num,
                                       double max_range){
     std::vector<DistPoint> candidates;
@@ -76,7 +135,7 @@ bool SparseVoxelGrid::GetClosestPoint(const PointType& point,
         std::nth_element(candidates.begin(), candidates.begin() + max_num - 1, candidates.end());
         candidates.resize(max_num);
     }
-    std::nth_element(candidates.begin(), candidates.begin(), candidates.end());
+    std::sort(candidates.begin(), candidates.begin());
 
     closest_point.clear();
     closest_point.reserve(candidates.size());
@@ -91,36 +150,16 @@ bool SparseVoxelGrid::GetClosestPoint(const PointType& point,
     return true;
 }
 
-void SparseVoxelGrid::AddPoints(const PointVector& points) {
-  if (future_.valid())
-    future_.get();
+void IVdb::AddPoints(const PointVector &points)
+{
   const double map_resolution = std::sqrt(voxel_size_ * voxel_size_ / max_points_per_voxel_);
+  std::unordered_set<Bonxai::CoordT> voxel_coords;
   auto accessor = map_.createAccessor();
-  std::for_each(points.cbegin(), points.cend(), [&](const PointType& point) {
+  std::for_each(points.cbegin(), points.cend(), [&](const PointType &point)
+                {
     Eigen::Vector3d p(point.x, point.y, point.z);
-    const auto voxel_coordinates = map_.posToCoord(p);
-
-    auto it = lru_map_.find(voxel_coordinates);
-    if (it != lru_map_.end()) {
-      // Move to front
-      lru_cache_.splice(lru_cache_.begin(), lru_cache_, it->second);
-    } else {
-      // Add to front
-      lru_cache_.push_front(voxel_coordinates);
-      lru_map_[voxel_coordinates] = lru_cache_.begin();
-
-      // Check for eviction
-      if (lru_map_.size() > capacity_) {
-        // Get key to evict
-        const Bonxai::CoordT key_to_evict = lru_cache_.back();
-        lru_cache_.pop_back();
-        lru_map_.erase(key_to_evict);
-        accessor.setCellOff(key_to_evict);
-        update_count++;
-      }
-    }
-
-    VoxelBlock* voxel_points = accessor.value(voxel_coordinates, /*create_if_missing=*/true);
+    const auto voxel_coordinate = map_.posToCoord(p);
+    VoxelBlock* voxel_points = accessor.value(voxel_coordinate, /*create_if_missing=*/true);
     if (voxel_points->size() == max_points_per_voxel_ ||
         std::any_of(voxel_points->cbegin(), voxel_points->cend(),
                     [&](const auto& voxel_point) { return (voxel_point - p).norm() < map_resolution; })) {
@@ -128,22 +167,161 @@ void SparseVoxelGrid::AddPoints(const PointVector& points) {
     }
     voxel_points->reserve(max_points_per_voxel_);
     voxel_points->emplace_back(p);
-  });
-  if (update_count > 1000)
-  {
-    future_ = std::async([&]()
-                         { map_.releaseUnusedMemory(); });
-  }
+    voxel_coords.insert(map_.getRootKey(voxel_coordinate)); });
+
+  UpdateLRU(voxel_coords);
 }
 
-std::vector<Eigen::Vector3d> SparseVoxelGrid::Pointcloud() const {
+// bool IVdb::GetClosestPoint(const PointType &point,
+//                                       PointVector &closest_point, int max_num,
+//                                       double max_range)
+// {
+//   Eigen::Vector3d pt(point.x, point.y, point.z);
+//   const Bonxai::CoordT voxel = map_.posToCoord(pt);
+
+//   auto custom_comp = [](const std::pair<double, VoxelBlock> &lhs, const std::pair<double, VoxelBlock> &rhs)
+//   {
+//     return lhs.first > rhs.first;
+//   };
+
+//   std::priority_queue<std::pair<double, VoxelBlock>, std::vector<std::pair<double, VoxelBlock>>, decltype(custom_comp)> candidates(custom_comp);
+//   const double max_range_sq = max_range * max_range;
+
+//   // Define the 3x3x3 root grid search space around the center
+//   const int32_t root_step = 1 << 2^5;
+//   Bonxai::CoordT center_root_key = map_.getRootKey(voxel);
+//   const int32_t min_root_z = center_root_key.z - root_step;
+//   const int32_t max_root_z = center_root_key.z + root_step;
+//   const int32_t min_root_y = center_root_key.y - root_step;
+//   const int32_t max_root_y = center_root_key.y + root_step;
+//   const int32_t min_root_x = center_root_key.x - root_step;
+//   const int32_t max_root_x = center_root_key.x + root_step;
+
+//   for (int32_t z = min_root_z; z <= max_root_z; z += root_step)
+//   {
+//     for (int32_t y = min_root_y; y <= max_root_y; y += root_step)
+//     {
+//       for (int32_t x = min_root_x; x <= max_root_x; x += root_step)
+//       {
+//         CoordT root_key = {x, y, z};
+//         auto root_it = map_.rootMap().find(root_key);
+//         auto &inner_grid = root_it->second;
+//         for (auto inner_it = inner_grid.mask().beginOn(); inner_it; ++inner_it)
+//         {
+//           const int32_t inner_index = *inner_it;
+//           auto &leaf_grid = inner_grid.cell(inner_index);
+//           if (!leaf_grid)
+//             continue;
+//           for (auto leaf_it = leaf_grid->mask().beginOn(); leaf_it; ++leaf_it)
+//           {
+//             const uint32_t leaf_index = *leaf_it;
+//             const VoxelBlock &voxel_data = leaf_grid->cell(leaf_index);
+//             double dist_sq = (voxel_data[0] - point.x) * (voxel_data[0] - point.x) +
+//                              (voxel_data[1] - point.y) * (voxel_data[1] - point.y) +
+//                              (voxel_data[2] - point.z) * (voxel_data[2] - point.z);
+//             if (dist_sq <= max_range_sq)
+//             {
+//               if (candidates.size() < max_num)
+//               {
+//                 candidates.push({dist_sq, voxel_data});
+//               }
+//               else if (dist_sq < candidates.top().first)
+//               {
+//                 candidates.pop();
+//                 candidates.push({dist_sq, voxel_data});
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   closest_point.clear();
+//   closest_point.reserve(candidates.size());
+
+//   while (!candidates.empty())
+//   {
+//     const auto &candidate = candidates.top();
+//     PointType point;
+//     point.x = candidate.second[0];
+//     point.y = candidate.second[1];
+//     point.z = candidate.second[2];
+//     closest_point.push_back(point);
+//     candidates.pop();
+//   }
+
+//   return true;
+// }
+
+// void IVdb::AddPoints(const PointVector &points)
+// {
+//   std::unordered_set<Bonxai::CoordT> voxel_coords;
+//   auto accessor = map_.createAccessor();
+//   std::for_each(points.cbegin(), points.cend(), [&](const PointType &point)
+//                 {
+//     Eigen::Vector3d p(point.x, point.y, point.z);
+//     const auto voxel_coordinate = map_.posToCoord(p);
+//     if(accessor.value(voxel_coordinate, false) == nullptr){
+//       accessor.setValue(voxel_coordinate, p);
+//     }
+//     voxel_coords.insert(map_.getRootKey(voxel_coordinate)); });
+//   // UpdateLRU(voxel_coords);
+// }
+
+void IVdb::UpdateLRU(const std::unordered_set<Bonxai::CoordT>& voxel_coords) {
+    std::vector<Bonxai::CoordT> delete_voxel_coords;
+    delete_voxel_coords.reserve(voxel_coords.size());
+    std::for_each(voxel_coords.cbegin(), voxel_coords.cend(), [&](const Bonxai::CoordT& voxel_coord) {
+        auto it = lru_map_.find(voxel_coord);
+        if (it != lru_map_.end()) {
+            // Move to front
+            lru_cache_.splice(lru_cache_.begin(), lru_cache_, it->second);
+        } else {
+            // Add to front
+            lru_cache_.push_front(voxel_coord);
+            lru_map_[voxel_coord] = lru_cache_.begin();
+
+            // Check for eviction
+            if (lru_map_.size() > capacity_) {
+                // Get key to evict
+                const Bonxai::CoordT key_to_evict = lru_cache_.back();
+                lru_cache_.pop_back();
+                lru_map_.erase(key_to_evict);
+                delete_voxel_coords.push_back(key_to_evict);
+            }
+        }
+    });
+
+    if (!delete_voxel_coords.empty()) {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        std::for_each(delete_voxel_coords.cbegin(), delete_voxel_coords.cend(), [&](const Bonxai::CoordT& voxel_coord) {
+            auto root_it = map_.rootMap().find(voxel_coord);
+            if (root_it != map_.rootMap().end()) {
+                auto& inner_grid = root_it->second;
+                for (auto inner_it = inner_grid.mask().beginOn(); inner_it; ++inner_it) {
+                    const int32_t inner_index = *inner_it;
+                    auto& leaf_grid = inner_grid.cell(inner_index);
+                    if (leaf_grid->mask().isOff()) {
+                        inner_grid.mask().setOff(inner_index);
+                        leaf_grid.reset();
+                    }
+                }
+                map_.rootMap().erase(voxel_coord);
+            }
+        });
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto insertion_time_ivox2 = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+        std::cout << "  UpdateLRU " << delete_voxel_coords.size() << " : "<< insertion_time_ivox2.count() << " ms" << std::endl;
+    }
+}
+
+std::vector<Eigen::Vector3d> IVdb::Pointcloud() const
+{
   std::vector<Eigen::Vector3d> point_cloud;
   point_cloud.reserve(map_.activeCellsCount() * max_points_per_voxel_);
-  map_.forEachCell([&point_cloud, this](const VoxelBlock& block, const auto&) {
-    point_cloud.insert(point_cloud.end(), block.cbegin(), block.cend());
-  });
-  std::cout<<map_.memUsage()<<std::endl;
+  map_.forEachCell([&point_cloud, this](const VoxelBlock &block, const auto &)
+                   { point_cloud.insert(point_cloud.end(), block.cbegin(), block.cend()); });
+  std::cout << map_.memUsage() << std::endl;
   return point_cloud;
 }
-
-} // namespace rko_lio::core
--- sparse_voxel_grid/sparse_voxel_grid.hpp
+++ sparse_voxel_grid/sparse_voxel_grid.hpp
@@ -1,38 +1,14 @@
-// straight copy from kinematic_icp, thanks Tiziano
-// MIT License
+#ifndef IVDB_H
+#define IVDB_H
 
-// Copyright (c) 2024 Tiziano Guadagnino, Benedikt Mersch, Ignacio Vizzo, Cyrill
-// Stachniss.
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-#pragma once
 #include <Eigen/Core>
 #include <vector>
 #include <list>
 #include <unordered_map>
-#include <memory>
-#include <future>
+#include <unordered_set>
 #include <pcl/common/centroid.h>
 
 #include "bonxai/bonxai.hpp"
-
-namespace rko_lio::core {
 
 using VoxelBlock = std::vector<Eigen::Vector3d>;
 using PointType = pcl::PointXYZ;
@@ -45,10 +21,10 @@ struct DistPoint {
     bool operator<(const DistPoint& other) const { return dist_sq < other.dist_sq; }
 };
 
-class SparseVoxelGrid {
+class IVdb {
   public:
-    explicit SparseVoxelGrid(const double voxel_size, const unsigned int max_points_per_voxel, const size_t capacity);
-    ~SparseVoxelGrid() = default;
+    explicit IVdb(const double voxel_size, const unsigned int max_points_per_voxel, const size_t capacity);
+    ~IVdb() = default;
     
     void Clear() { 
       map_.clear(Bonxai::ClearOption::CLEAR_MEMORY); 
@@ -64,12 +40,12 @@ class SparseVoxelGrid {
     double voxel_size_;
     unsigned int max_points_per_voxel_;
     size_t capacity_;
-    size_t update_count;
     Bonxai::VoxelGrid<VoxelBlock> map_;
     
     std::list<Bonxai::CoordT> lru_cache_;
     std::unordered_map<Bonxai::CoordT, std::list<Bonxai::CoordT>::iterator> lru_map_;
-    std::future<void> future_;
+
+    void UpdateLRU(const std::unordered_set<Bonxai::CoordT> &voxel_coords);
 };
 
-} // namespace rko_lio::core
+#endif
